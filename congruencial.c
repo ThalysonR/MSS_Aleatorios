75a5be41afe2d00c5cf1cf17f8f899e556c75ba3
Thalyson Rocha <trocha@alfa.corp>
2017-06-03 14:12:58 -0400

    Ajuste

--- congruencial.c
+++ congruencial.c
@@ -4,8 +4,8 @@
 
 time_t _seed = 0;
 
-int congruencial(int maximo){
-		_seed = (13*_seed+7) % maximo;
+long int congruencial(int maximo){
+		_seed = (17*_seed+43) % maximo;
 		return _seed;
 	}
 
@@ -13,14 +13,14 @@ int main(int argc, char **argv)
 {
 	int numeros = atoi(argv[1]), maximo = atoi(argv[2]);
 	int falta = numeros;
-	_seed = time(NULL)/2;
+	_seed = time(NULL)/100;
 	
 	numeros = numeros % 2 == 0 ? numeros/2 : numeros/2 + 1;
 	
 	for(int i = 0; i < numeros; i++){
 		for(int j = 0; j < 2; j++){
 			if(falta > 0){
-				printf("%d\t",congruencial(maximo));
+				printf("%ld\t",congruencial(maximo));
 				falta--;
 			}
 		}
